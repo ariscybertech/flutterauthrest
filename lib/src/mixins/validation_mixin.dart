83f5b023cfef34eaedd4a2601c044eab7cc5a592
ariscybertech <ariscybertech@gmail.com>
2022-04-13 11:24:55 +0500

    first commit

---
+++ lib/src/mixins/validation_mixin.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+
+class ValidationMixin {
+  // with Provider state
+  final validatorUsername = new StreamTransformer<String, String>.fromHandlers(
+    handleData: (username, sink) {
+      // if (email.isEmpty) {
+      //   sink.addError('Email is required');
+      // } else if (!email.contains('@')) {
+      //   sink.addError('Please enter a valid email');
+      // } else if (email.contains(' ')) {
+      //   sink.addError('Space not allowd! 😏');
+      // } else {
+      //   sink.add(email);
+      // }
+      if (ValidationMixin()._validateUsername(username)) {
+        sink.addError('Username is not valid');
+      } else {
+        sink.add(username);
+      }
+    },
+  );
+
+  final validatorPassword = new StreamTransformer<String, String>.fromHandlers(
+    handleData: (password, sink) {
+      if (!ValidationMixin()._validatePassword(password)) {
+        sink.addError('Password is not valid!');
+      } else {
+        sink.add(password);
+      }
+    },
+  );
+  // without Provider state
+  bool _validateUsername(String username) {
+    var regExp = new RegExp(
+        r"^[-a-zA-Z0-9_]+$");
+    // if (email.isEmpty) {
+    //   return false;
+    // }
+    if (regExp.hasMatch(username)) {
+      return false;
+    }
+    return true;
+  }
+
+  bool _validatePassword(String password) {
+    var regExp = new RegExp(r'^(?=.*?[A-Z][a-z]).{8,}$');
+    // if (password.isEmpty) {
+    //   return false;
+    // }
+    // if (password.length < 8) {
+    //   return false;
+    // }
+    if (regExp.hasMatch(password)) {
+      return false;
+    }
+    return true;
+  }
+}
